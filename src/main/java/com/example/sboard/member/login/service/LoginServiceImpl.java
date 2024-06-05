8d75021155f2a3a1f981c67df688aaaad79a3574
wsk0715 <leindey@gmail.com>
2024-06-05 16:24:21 +0900

    feat: LoginServiceImpl 어노테이션 추가 - @Transactional

--- src/main/java/com/example/sboard/member/login/service/LoginServiceImpl.java
+++ src/main/java/com/example/sboard/member/login/service/LoginServiceImpl.java
@@ -3,8 +3,10 @@ package com.example.sboard.member.login.service;
 import com.example.sboard.member.domain.Member;
 import com.example.sboard.member.login.mapper.LoginMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class LoginServiceImpl implements LoginService {
 	private final LoginMapper loginMapper;
 
